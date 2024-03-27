50fe8ed1abb8cb3009b465752cfb04fc3a3d50c6
Ataix <atai_ks@yahoo.com>
2024-03-27 20:05:13 +0600

    Test cases for auth were developed. Altered order model and serializers

---
+++ src/account/tests/test_auth.py
@@ -0,0 +1,57 @@
+import json
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+client = APIClient()
+User = get_user_model()
+
+
+class TestOrderViews(TestCase):
+    """
+    Test product's list view
+    """
+    def setUp(self):
+        self.test_user = User.objects.create_user(
+            username='test_user_1', password='<PASSWORD>'
+        )
+        refresh = RefreshToken.for_user(self.test_user)
+        self.auth = {"HTTP_AUTHORIZATION": f'Bearer {refresh.access_token}'}
+
+    def test_order_list_valid_access(self):
+        response = client.get(
+            '/api/v1/order/list/',
+            **self.auth
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_order_list_invalid_access(self):
+        response = client.get(
+            '/api/v1/order/list/',
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_product_create_valid_access(self):
+        response = client.post(
+            f'/api/v1/product/create/',
+            **self.auth
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_product_create_invalid_access(self):
+        response = client.post(
+            f'/api/v1/product/create/',
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_product_list_anonymous_access(self):
+        response = client.get(
+            f'/api/v1/product/list/',
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
--- src/product/tests/test_models.py
+++ src/product/tests/test_models.py
@@ -1,69 +1,69 @@
-# from django.contrib.auth import get_user_model
-# from django.test import TestCase
-#
-# from ..models import Product, Review
-#
-#
-# User = get_user_model()
-#
-#
-# class TestProductReviewModel(TestCase):
-#     """
-#     Test product's model
-#     """
-#     def setUp(self):
-#         self.product_1 = Product.objects.create(
-#             name='Test Product 1',
-#             description='Test Description 1',
-#             category='Test Category 1',
-#             price=322.0,
-#             quantity=10,
-#         )
-#         self.product_2 = Product.objects.create(
-#             name='Test Product 2',
-#             description='Test Description 2',
-#             category='Test Category 2',
-#             price=109.0,
-#             quantity=9,
-#         )
-#         self.user_1 = User.objects.create(
-#             username='user1',
-#             password='<PASSWORD>'
-#         )
-#         self.review_1 = Review.objects.create(
-#             product=self.product_1,
-#             text='Test Review 1',
-#             author=self.user_1,
-#             rate=5
-#         )
-#         self.review_2 = Review.objects.create(
-#             product=self.product_2,
-#             text='Test Review 2',
-#             author=self.user_1,
-#             rate=5
-#         )
-#
-#     def test_product_creation(self):
-#         product_1 = Product.objects.get(name='Test Product 1')
-#         product_2 = Product.objects.get(name='Test Product 2')
-#         self.assertEqual(self.product_1.name, product_1.name)
-#         self.assertEqual(self.product_1.description, product_1.description)
-#         self.assertEqual(self.product_1.category, product_1.category)
-#         self.assertEqual(self.product_1.price, product_1.price)
-#         self.assertEqual(self.product_1.quantity, product_1.quantity)
-#         self.assertEqual(self.product_2.name, product_2.name)
-#         self.assertEqual(self.product_2.description, product_2.description)
-#         self.assertEqual(self.product_2.category, product_2.category)
-#         self.assertEqual(self.product_2.price, product_2.price)
-#         self.assertEqual(self.product_2.quantity, product_2.quantity)
-#
-#         review_1 = Review.objects.get(id=self.review_1.id)
-#         review_2 = Review.objects.get(id=self.review_2.id)
-#         self.assertEqual(self.review_1.product, review_1.product)
-#         self.assertEqual(self.review_1.author, review_1.author)
-#         self.assertEqual(self.review_1.rate, review_1.rate)
-#         self.assertEqual(self.review_1.text, review_1.text)
-#         self.assertEqual(self.review_2.product, review_2.product)
-#         self.assertEqual(self.review_2.author, review_2.author)
-#         self.assertEqual(self.review_2.rate, review_2.rate)
-#         self.assertEqual(self.review_2.text, review_2.text)
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from ..models import Product, Review
+
+
+User = get_user_model()
+
+
+class TestProductReviewModel(TestCase):
+    """
+    Test product's model
+    """
+    def setUp(self):
+        self.product_1 = Product.objects.create(
+            name='Test Product 1',
+            description='Test Description 1',
+            category='Test Category 1',
+            price=322.0,
+            quantity=10,
+        )
+        self.product_2 = Product.objects.create(
+            name='Test Product 2',
+            description='Test Description 2',
+            category='Test Category 2',
+            price=109.0,
+            quantity=9,
+        )
+        self.user_1 = User.objects.create(
+            username='user1',
+            password='<PASSWORD>'
+        )
+        self.review_1 = Review.objects.create(
+            product=self.product_1,
+            text='Test Review 1',
+            author=self.user_1,
+            rate=5
+        )
+        self.review_2 = Review.objects.create(
+            product=self.product_2,
+            text='Test Review 2',
+            author=self.user_1,
+            rate=5
+        )
+
+    def test_product_creation(self):
+        product_1 = Product.objects.get(name='Test Product 1')
+        product_2 = Product.objects.get(name='Test Product 2')
+        self.assertEqual(self.product_1.name, product_1.name)
+        self.assertEqual(self.product_1.description, product_1.description)
+        self.assertEqual(self.product_1.category, product_1.category)
+        self.assertEqual(self.product_1.price, product_1.price)
+        self.assertEqual(self.product_1.quantity, product_1.quantity)
+        self.assertEqual(self.product_2.name, product_2.name)
+        self.assertEqual(self.product_2.description, product_2.description)
+        self.assertEqual(self.product_2.category, product_2.category)
+        self.assertEqual(self.product_2.price, product_2.price)
+        self.assertEqual(self.product_2.quantity, product_2.quantity)
+
+        review_1 = Review.objects.get(id=self.review_1.id)
+        review_2 = Review.objects.get(id=self.review_2.id)
+        self.assertEqual(self.review_1.product, review_1.product)
+        self.assertEqual(self.review_1.author, review_1.author)
+        self.assertEqual(self.review_1.rate, review_1.rate)
+        self.assertEqual(self.review_1.text, review_1.text)
+        self.assertEqual(self.review_2.product, review_2.product)
+        self.assertEqual(self.review_2.author, review_2.author)
+        self.assertEqual(self.review_2.rate, review_2.rate)
+        self.assertEqual(self.review_2.text, review_2.text)
